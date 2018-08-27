3460f04590cc2cbed7dddb8374e7c13ed8dff820
ChungNQ <chungnguyenquan@admicro.vn>
2018-08-27 17:13:34 +0700

    Learn Router and param of router

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,24 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', { title: 'Hello Express' });
+});
+
+router.get('/about', function (req, res, next) {
+    res.render('about', { content: 'About Page' });
+});
+
+router.get('/check-tam-giac/:canhA/:canhB/:canhC', function (req, res, next) {
+    let { canhA } = req.params;
+    let { canhB } = req.params;
+    let { canhC } = req.params;
+
+    res.render('math/triangle', {
+        canhA: Number(canhA),
+        canhB: Number(canhB),
+        canhC: Number(canhC)
+    });
 });
 
 module.exports = router;
