6c22f0a8d919548dca408f93751a2a0d6bc00f7e
ChungNQ <chungnguyenquan@admicro.vn>
2018-08-29 15:54:09 +0700

    add more features category

--- routes/index.js
+++ routes/index.js
@@ -24,8 +24,43 @@ router.get('/check-tam-giac/:canhA/:canhB/:canhC', function (req, res, next) {
 });
 
 router.get('/list-categories', async function(req, res, next) {
-    var listCategories = await Category.find();
+    let listCategories = await Category.find();
     res.render('category/list_categories', {listCategories} );
 });
+
+router.get('/form-update/:cateId?', async function ( req, res, next) {
+    let { cateId } = req.params;
+    let category = {};
+    if (cateId) {
+        category = await Category.findOne({ id: cateId })
+    } ;
+    
+    res.render('category/form_update', { category });
+});
+
+router.get('/form-create', async function ( req, res, next) {
+    res.render('category/form_create');
+});
+
+router.get('/action-remove/:cateId', async function ( req, res, next) {
+    let { cateId } = req.params;
+    if (cateId) {
+        await Category.findOneAndRemove({ id: cateId })
+    } ;
+
+    res.redirect('/list-categories');
+});
+
+router.post('/action-create', async function ( req, res, next) {
+    // let { cateId } = req.params;
+    // if (cateId) {
+    //     await Category.findOneAndRemove({ id: cateId })
+    // } ;
+
+    console.log(req.body);
+
+    // res.redirect('/list-categories');
+});
+
 
 module.exports = router;
